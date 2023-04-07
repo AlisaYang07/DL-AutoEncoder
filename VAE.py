0965715433d432bccb75192856aff986d7d39c37
AlisaYang07 <alisajiexuyang@gmail.com>
2023-04-06 22:51:11 -0400

    clean up the code

--- VAE.py
+++ VAE.py
@@ -2,27 +2,18 @@ import torch.nn as nn
 import torch
 ##https://github.com/Jackson-Kang/Pytorch-VAE-tutorial/blob/master/01_Variational_AutoEncoder.ipynb
 class VAE(nn.Module):
-    def __init__(self,encoder_model, decoder_model, z_dim = 512, latent_dim = 64):
+    def __init__(self,encoder_model, decoder_model, in_dim = 512, latent_dim = 64):
         super().__init__()
         self.encoder = encoder_model
         self.decoder = decoder_model
-
-        self.z_size = latent_dim
-        self.feature_volume = z_dim
-
-        self.FC_mean  = nn.Linear(z_dim , latent_dim)
-        self.FC_var   = nn.Linear (z_dim , latent_dim)
-
-        # projection
-        # self.relu = nn.ReLU()
-
+        self.FC_mean  = nn.Linear(in_dim , latent_dim)
+        self.FC_var   = nn.Linear (in_dim , latent_dim)
         self.LeakyReLU = nn.LeakyReLU(0.2)
-        
+
     # def q(self, encoded):
     #     unrolled = encoded.view(-1, self.feature_volume)
     #     return self.FC_mean(unrolled), self.FC_var(unrolled)
 
-    
     def reparameterization(self, mean, var):
         epsilon = torch.randn_like(var).cuda()       # sampling epsilon        
         z = mean + var *epsilon                          # reparameterization trick
@@ -36,7 +27,6 @@ class VAE(nn.Module):
 
         # mean, log_var = self.q(encoder)
         z = self.reparameterization(mean, torch.exp(0.5 * log_var))
-        
         out = self.decoder(z)
         # out = self.sigmoid(decoder)
 
